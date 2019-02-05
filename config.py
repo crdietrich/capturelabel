f94630ab16d26d73825b589790afa9b377940fa8
Colin Dietrich <repos@wildjuniper.com>
2019-02-05 11:35:08 -0800

    Update for streaming

--- config.py
+++ config.py
@@ -6,10 +6,12 @@ from sys import platform
 # ===== File Saving Location =====
 if platform == "linux" or platform == "linux2":
     data_dir = "/home/user/labeled_images/"
+    stream_dir = "/home/colin/data/stream_images/"
 elif platform == "darwin": # OS X
 	pass
 elif platform == "win32":
     data_dir = "C:\\Users\\user\\labeled_images\\"
+    stream_dir = "C:\\Users\\user\\stream_images\\"
 
 # ===== File Save Naming =====
 # this will be included in the file name so keep it short
@@ -31,7 +33,7 @@ buttons = {"joint": default_button_css,
 		   "vault_lid": default_button_css,
 		   "vertical_crack": default_button_css,
 		   "alligator_crack": default_button_css,
-           "brick/cobble": default_button_css
+           "brick/cobble": default_button_css,
 		   "other": default_button_css
 		   }
 		   #"background-image: url('81x100_rabbit.png');"+default_css}
@@ -40,5 +42,4 @@ buttons = {"joint": default_button_css,
 # Select camera to access,
 # this depends on computer and number of cameras installed.
 # If there is only a USB camera, camera_id = 0
-camera_id = 2
-
+camera_id = 1
--- main.py
+++ main.py
@@ -5,10 +5,16 @@ import sys
 from PyQt5.QtWidgets import QApplication
 
 import config
-import gui
 
 
-app = QApplication(sys.argv)
-window = gui.Window(config.buttons)
-window.show()
-sys.exit(app.exec_())
+if sys.argv[1] == 's':
+    from camera_capture import CAM
+    cam = CAM(cam_id=config.camera_id, description=config.description)
+    cam.save_directory = config.stream_dir
+    cam.stream_image_files(n=0, delay=0, stats=False)
+else:
+    import gui
+    app = QApplication(sys.argv)
+    window = gui.Window(config.buttons)
+    window.show()
+    sys.exit(app.exec_())
